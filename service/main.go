dd6935c7e872a5219b6cf827e6da1fe940ee6190
Brucegx <guoxi.bruce@gmail.com>
2018-02-05 22:40:00 -0500

    change url

--- service/main.go
+++ service/main.go
@@ -19,7 +19,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://104.196.102.242:9200"
+	ES_URL = "http://35.227.105.35:9200"
 )
 
 
